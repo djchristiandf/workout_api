13fbbbca4ac639905cca62b32e3f923296b9edbf
djchristiandf <djchristiandf@gmail.com>
2024-06-06 23:28:51 -0300

    projeto finalizado com as solicitacoes pedidas no final

--- app/atleta/controller.py
+++ app/atleta/controller.py
@@ -1,13 +1,171 @@
-from fastapi import APIRouter, status, Body
+from datetime import datetime
+from uuid import uuid4
 
-from app.atleta.schemas import AtletaIn
+from fastapi import APIRouter, status, Body, HTTPException
+from pydantic import UUID4
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from fastapi_pagination import Page, paginate, add_pagination
+
+from app import CategoriaModel, AtletaModel, CentroTreinamentoModel
+from app.atleta.schemas import AtletaIn, AtletaOut, AtletaUpdate
 from app.contrib.dependencies import DatabaseDependency
 
 router = APIRouter()
 
+@router.post(
+    '/',
+    summary='Criar um novo atleta',
+    status_code=status.HTTP_201_CREATED,
+    response_model=AtletaOut
+)
+async def post(
+        db_session: DatabaseDependency,
+        atleta_in: AtletaIn = Body(...)
+):
+    categoria_nome = atleta_in.categoria.nome
+    centro_treinamento_nome = atleta_in.centro_treinamento.nome
 
-@router.post("/",
-             summary='Criar um novo atleta',
-             status_code=status.HTTP_201_CREATED)
-async def post(db_session: DatabaseDependency, atleta_in: AtletaIn = Body(...)):
-    pass
+    categoria = (await db_session.execute(
+        select(CategoriaModel).filter_by(nome=categoria_nome))
+                 ).scalars().first()
+
+    if not categoria:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'A categoria {categoria_nome} não foi encontrada.'
+        )
+
+    centro_treinamento = (await db_session.execute(
+        select(CentroTreinamentoModel).filter_by(nome=centro_treinamento_nome))
+                          ).scalars().first()
+
+    if not centro_treinamento:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'O centro de treinamento {centro_treinamento_nome} não foi encontrado.'
+        )
+    try:
+        atleta_out = AtletaOut(id=uuid4(), created_at=datetime.utcnow(), **atleta_in.model_dump())
+        atleta_model = AtletaModel(**atleta_out.model_dump(exclude={'categoria', 'centro_treinamento'}))
+
+        atleta_model.categoria_id = categoria.pk_id
+        atleta_model.centro_treinamento_id = centro_treinamento.pk_id
+
+        db_session.add(atleta_model)
+        await db_session.commit()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER,
+            detail=f'Já existe um atleta cadastrado com o CPF: {atleta_in.cpf}'
+        )
+
+    return atleta_out
+
+
+@router.get(
+    '/',
+    summary='Consultar todos os Atletas',
+    status_code=status.HTTP_200_OK,
+    response_model=Page[AtletaOut],
+)
+async def query(
+        db_session: DatabaseDependency,
+        nome: str = None,
+        cpf: str = None
+):
+    query = select(AtletaModel)
+
+    if nome:
+        query = query.filter(AtletaModel.nome.ilike(f'%{nome}%'))
+
+    if cpf:
+        query = query.filter(AtletaModel.cpf == cpf)
+
+    atletas = await db_session.execute(query)
+    atletas_out = [
+        AtletaOut(
+            id=atleta.id,
+            nome=atleta.nome,
+            cpf=atleta.cpf,
+            centro_treinamento=AtletaOut.CentroTreinamento(
+                nome=atleta.centro_treinamento.nome
+            ),
+            categoria=AtletaOut.Categoria(
+                nome=atleta.categoria.nome
+            )
+        )
+        for atleta in atletas.scalars().all()
+    ]
+
+    return paginate(atletas_out)
+
+
+@router.get(
+    '/{id}',
+    summary='Consulta um Atleta pelo id',
+    status_code=status.HTTP_200_OK,
+    response_model=AtletaOut,
+)
+async def get(id: UUID4, db_session: DatabaseDependency) -> AtletaOut:
+    atleta: AtletaOut = (
+        await db_session.execute(select(AtletaModel).filter_by(id=id))
+    ).scalars().first()
+
+    if not atleta:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Atleta não encontrado no id: {id}'
+        )
+
+    return atleta
+
+
+@router.patch(
+    '/{id}',
+    summary='Editar um Atleta pelo id',
+    status_code=status.HTTP_200_OK,
+    response_model=AtletaOut,
+)
+async def patch(id: UUID4, db_session: DatabaseDependency,
+                atleta_up: AtletaUpdate = Body(...)) -> AtletaOut:
+    atleta: AtletaOut = (
+        await db_session.execute(select(AtletaModel).filter_by(id=id))
+    ).scalars().first()
+
+    if not atleta:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Atleta não encontrado no id: {id}'
+        )
+
+    atleta_update = atleta_up.model_dump(exclude_unset=True)
+    for key, value in atleta_update.items():
+        setattr(atleta, key, value)
+
+    await db_session.commit()
+    await db_session.refresh(atleta)
+
+    return atleta
+
+
+@router.delete(
+    '/{id}',
+    summary='Deletar um Atleta pelo id',
+    status_code=status.HTTP_204_NO_CONTENT
+)
+async def delete(id: UUID4, db_session: DatabaseDependency) -> None:
+    atleta: AtletaOut = (
+        await db_session.execute(select(AtletaModel).filter_by(id=id))
+    ).scalars().first()
+
+    if not atleta:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Atleta não encontrado no id: {id}'
+        )
+
+    await db_session.delete(atleta)
+    await db_session.commit()
+
+add_pagination(router)
--- app/atleta/schemas.py
+++ app/atleta/schemas.py
@@ -1,5 +1,8 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import Field, PositiveFloat
+
+from app.categorias.schemas import CategoriaIn
+from app.centro_treinamento.schemas import CentroTreinamentoAtleta
 from app.contrib.schemas import BaseSchema, OutMixin
 
 
@@ -10,7 +13,8 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
     altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.70)]
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
-
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamentoAtleta, Field(description='Centro de treinamento do atleta')]
 
 class AtletaIn(Atleta):
     pass
@@ -18,3 +22,8 @@ class AtletaIn(Atleta):
 
 class AtletaOut(Atleta, OutMixin):
     pass
+
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
+    idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
